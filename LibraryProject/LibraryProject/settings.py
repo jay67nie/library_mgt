cadb8378e59b915895ac261db71bce6e8e44e45f
Jerome Ssenyonjo <jeromessenyonjo@gmail.com>
2022-07-19 16:56:29 +0300

    Heroku

--- LibraryProject/LibraryProject/settings.py
+++ LibraryProject/LibraryProject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-# STATIC_FILES = os.path.join(BASE_DIR, "staticfiles")
+STATIC_FILES = os.path.join(BASE_DIR, "staticfiles")
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
@@ -120,11 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATICFILES_DIRS = (
-#     STATIC_FILES,
-# )
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    STATIC_FILES,
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
